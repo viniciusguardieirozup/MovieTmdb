d06fcbfe2b1d217dd2d7fa9f73f39cccce885790
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2019-12-20 12:27:43 -0300

    Identation

--- app/src/main/java/com/example/movietmdb/retrofit/SearchResults.kt
+++ app/src/main/java/com/example/movietmdb/retrofit/SearchResults.kt
@@ -3,7 +3,12 @@ package com.example.movietmdb.retrofit
 import com.google.gson.annotations.SerializedName
 
 data class SearchResults(
-    @SerializedName("page") val page: Int, @SerializedName("results") val results: ArrayList<DataMovies>, @SerializedName(
-        "total_results"
-    ) val totalResults: Int, @SerializedName("total_pages") val totalPages: Int
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: ArrayList<DataMovies>,
+    @SerializedName("total_results")
+    val totalResults: Int,
+    @SerializedName("total_pages")
+    val totalPages: Int
 )
