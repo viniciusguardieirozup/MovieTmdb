0e3581df6216110d59b3503729c64dd5d6803a7f
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2019-12-20 09:59:37 -0300

    Created the function to get the movie name inform by the user

--- app/src/main/java/com/example/movietmdb/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/movietmdb/fragments/SearchFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.movietmdb.R
+import kotlinx.android.synthetic.main.search_movies_layout.*
 
 class SearchFragment : Fragment() {
 
@@ -21,6 +24,26 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return View.inflate(context, R.layout.search_movies_layout, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        searchMovie.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                getTextToSearch()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun getTextToSearch() {
+        searchMovie.clearFocus()
+        val movieName = searchMovie.query.toString()
+        searchMovie.setQuery("", false)
+        Toast.makeText(context, movieName, Toast.LENGTH_SHORT).show()
     }
 
 }
