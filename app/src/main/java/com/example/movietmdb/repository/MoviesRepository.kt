6d05930d8b8c72e6ac3b0a962cc72ab317d8b484
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2020-02-05 10:02:58 -0300

    change the repository name

---
+++ app/src/main/java/com/example/movietmdb/repository/MoviesRepository.kt
@@ -0,0 +1,30 @@
+package com.example.movietmdb.repository
+
+import com.example.movietmdb.repository.db.DAO.MovieDao
+import com.example.movietmdb.repository.db.entity.MovieData
+import com.example.movietmdb.repository.retrofit.GenresList
+import com.example.movietmdb.repository.retrofit.MoviesAPI
+
+class MoviesRepository(private val moviesAPI: MoviesAPI, private val movieDAO: MovieDao) {
+
+    suspend fun insertMovie(movie: MovieData) {
+        movieDAO.insertMovie(movie)
+    }
+
+    suspend fun removeMovie(movie: MovieData) {
+        movieDAO.removeMovie(movie)
+    }
+
+    suspend fun getMovies(name: String, page: Int) =
+        moviesAPI.searchMoviesByUser(name, page)
+
+    suspend fun getFavMovies() = movieDAO.getAll()
+
+    suspend fun getSimilar(id: Int, page: Int) = moviesAPI.getSimilar(id, page)
+
+    suspend fun getMoviesByGenres(id: Int, page: Int) = moviesAPI.getMoviesByGenres(id, page)
+
+    suspend fun getGenres(): GenresList {
+        return moviesAPI.getGenres()
+    }
+}
