0bea5e6791ef8a2521ddb05fcf2a7d42d1fae751
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2020-02-12 14:07:40 -0300

    create the similar layouts

---
+++ app/src/main/java/com/example/movietmdb/recycler/holder/SimilarBodyViewHolder.kt
@@ -0,0 +1,48 @@
+package com.example.movietmdb.recycler.holder
+
+import android.app.Activity
+import android.content.Intent
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.movietmdb.R
+import com.example.movietmdb.databinding.ItemMovieLayoutBinding
+import com.example.movietmdb.databinding.ItemMovieSimilarLayoutBinding
+import com.example.movietmdb.features.description.ui.Description2Activity
+import com.example.movietmdb.recycler.data.MoviePresentation
+
+
+class SimilarBodyViewHolder(val binding: ItemMovieSimilarLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    private lateinit var movieItem: MoviePresentation
+
+    fun bind(
+        movieItem: MoviePresentation
+    ) {
+        binding.item = movieItem
+        this.movieItem = movieItem
+        if (movieItem.posterPath == null) {
+            binding.itemSimilarMovieImage.setImageResource(R.drawable.ic_not_found)
+        } else {
+            Glide.with(itemView.context)
+                .load(movieItem.posterPath)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(binding.itemSimilarMovieImage)
+        }
+        itemClick()
+    }
+
+    private fun itemClick() {
+        itemView.setOnClickListener {
+            val intent = Intent(itemView.context, Description2Activity::class.java)
+            movieItem.type = 0
+            intent.putExtra("movie", movieItem)
+            itemView.context.startActivity(intent)
+            (itemView.context as Activity).overridePendingTransition(
+                R.anim.slide_to_left_enter,
+                R.anim.slide_to_left_exit
+            )
+        }
+    }
+
+}
